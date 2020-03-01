12cf19efbe51f2b0a5e659d9d3d9355dbe4b1fc0
benkarenas <55295773+benkarenas@users.noreply.github.com>
2020-02-29 18:28:33 -0600

    Update main.R

--- main.R
+++ main.R
@@ -11,16 +11,16 @@ require(ROI.plugin.quadprog)
 quandl_api_key("mRJDZwn3giwAm1kowtFr")
 
 # Pull tickers from Excel file
-tickers = readxl::read_excel("tickers.xlsx", col_names = FALSE) %>% pull()
+tickers <- readxl::read_excel("tickers.xlsx", col_names = FALSE) %>% pull()
 
 # Add EOD prefix to tickers so they pull end of day prices from QUandl
-modified_tickers = sprintf("EOD/%s", tickers)
+modified_tickers <- sprintf("EOD/%s", tickers)
 
 # Pull EOD stock prices from Quandl between 2 dates, must be at least 3 years for it to work properly
-stock_prices = group_by(tq_get(modified_tickers, get = "quandl", from = "2016-01-01", to = "2020-01-01"), symbol)
+stock_prices <- group_by(tq_get(modified_tickers, get = "quandl", from = "2016-01-01", to = "2020-01-01"), symbol)
 
 # Calculate returns on the stocks over the time period
-stock_returns = stock_prices %>%
+stock_returns <- stock_prices %>%
   tq_transmute(select     = adj_close,
                mutate_fun = periodReturn,
                period     = "daily",
@@ -29,27 +29,28 @@ stock_returns = stock_prices %>%
   pivot_wider(names_from = symbol, values_from = returns)
 
 # Coerce the stock returns into a xts time series
-stock_returns = stock_returns %>% tk_xts(silent = TRUE)
+stock_returns <- stock_returns %>% tk_xts(silent = TRUE)
 
 # Create a portfolio
-init = portfolio.spec(assets = colnames(stock_returns))
+init <- portfolio.spec(assets = colnames(stock_returns))
 
 # Add the constraint on the minimum and maximum weight that a single stock can have
-init = add.constraint(portfolio = init, type = "box", min = 0.005, max = 0.1)
+init <- add.constraint(portfolio = init, type = "box", min = 0.005, max = 0.1)
 
 # Add the quadratic objective for the solver
-qu = add.objective(portfolio = init, type = "quadratic_utility", risk_aversion = 1)
+qu <- add.objective(portfolio = init, type = "quadratic_utility", risk_aversion = 1)
 
 # Optimize the portfolio based on the above constraints and objectives using quadratic programming
-opt_qu = optimize.portfolio(R = stock_returns, portfolio = qu, optimize_method = "quadprog")
+opt_qu <- optimize.portfolio(R = stock_returns, portfolio = qu, optimize_method = "quadprog")
 print(opt_qu)
 
 # Extract the optimized weights and put them in a data frame with the tickers
-weights = extractWeights(opt_qu) %>% as.data.frame()
-df = cbind(tickers, weights)
+weights <- extractWeights(opt_qu) %>% as.data.frame()
+df <- cbind(tickers, weights)
 
 # Export the final data frame to an Excel file
-wb = createWorkbook()
+wb <- createWorkbook()
 addWorksheet(wb, "Sheet1")
 writeData(wb, "Sheet1", df, colNames = FALSE)
 saveWorkbook(wb, "weights.xlsx", overwrite = TRUE)
+
